fe7098de8b369fa890013b164c3d7585127c3f58
Caixian-Wang <20108795@mail.wit.ie>
2025-02-06 00:45:10 +0000

    Demo optional properties and parameters.

---
+++ src/myTypes.d.ts
@@ -0,0 +1,26 @@
+export interface Friend {
+    name: string;
+    phone: string;
+    dob? : Date;   // New
+    age: number;
+    interests? : string[]   // New
+}
+
+export interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number
+    } 
+  }
+
+export interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
+export interface EmailContact {
+    name: string;
+    email: string
+}
